c1a65bebcc0b8d55841a12cb5e68717dde08057c
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2025-05-01 17:02:23 -0600

    test: :ambulance: test components
    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
-import { createContext } from "astro/middleware";
 
 const isProtectedRoute =  createRouteMatcher(["/historical(.*)"]);
 
-export const onRequest = clerkMiddleware((auth, context)=>{
+export const onRequest = clerkMiddleware((auth, context) =>{
     const {userId , redirectToSignIn } = auth();
 
     if(isProtectedRoute(context.request) && !userId ){
