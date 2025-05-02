dcd4c5fde05985e0910f8ddd29bc3794ad08117b
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2025-05-01 18:14:31 -0600

    docs: :bug: add return in middleware
    add return in middleware
    
    add return in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,10 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server";
 
 const isProtectedRoute =  createRouteMatcher(["/historical(.*)"]);
 
-export const onRequest = clerkMiddleware((auth, context) =>{
-    const {userId , redirectToSignIn } = auth();
+export const onRequest = clerkMiddleware((auth, context) => {
+    const { userId , redirectToSignIn } = auth();
 
     if(isProtectedRoute(context.request) && !userId ){
-        redirectToSignIn();
+       return redirectToSignIn();
     }
 });
